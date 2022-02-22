9a7b954b016e7da07905010e65d32ffa2acacc37
Alexzzl <805119233@qq.com>
2022-02-23 00:14:14 +0800

    Inheritance 2

--- contracts/Inheritance.sol
+++ contracts/Inheritance.sol
@@ -3,13 +3,21 @@
 pragma solidity ^0.8.4;
 
 contract A {
-    function getContractName() public pure virtual returns (string memory) {
-        return "A";
+
+    string public name;
+
+    constructor(string memory _name) {
+        name = _name;
+    }
+
+
+    function getContractName() public view virtual returns (string memory) {
+        return name;
     }
 }
 
-contract B is A {
-    function getContractName() public pure override(A) returns (string memory) {
-        return "B";
-    }
+contract B is A("B") {
+    // function getContractName() public pure override(A) returns (string memory) {
+    //     return "B";
+    // }
 }
