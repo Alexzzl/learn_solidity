8d351c51d4f10e59e84aa5e3d1ce2878879d3d6d
Alexzzl <805119233@qq.com>
2022-02-23 00:26:31 +0800

    inhertance 5

--- contracts/Inheritance.sol
+++ contracts/Inheritance.sol
@@ -18,11 +18,12 @@ contract A {
 
 // contract B is A("B") {
 contract B is A {
+    string public helloworld;
     // function getContractName() public pure override(A) returns (string memory) {
     //     return "B";
     // }
     // constructor() A("This is B"){
-    constructor(string memory _name) A(_name){
-
+    constructor(string memory _name, string memory _helloworld) A(_name){
+        helloworld = _helloworld;
     }
 }
