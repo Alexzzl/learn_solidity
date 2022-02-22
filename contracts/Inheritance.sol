2883bacab41c4e2a1e5d796d4a2dd372f2dc1c88
Alexzzl <805119233@qq.com>
2022-02-23 00:04:30 +0800

    Inheritance

---
+++ contracts/Inheritance.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract A {
+    function getContractName() public pure virtual returns (string memory) {
+        return "A";
+    }
+}
+
+contract B is A {
+    function getContractName() public pure override(A) returns (string memory) {
+        return "B";
+    }
+}
