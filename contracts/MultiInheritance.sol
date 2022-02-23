dffcf84cf0f8e73ef12bf93eefc6dba3a776f2dc
Alexzzl <805119233@qq.com>
2022-02-23 20:00:19 +0800

    more name

--- contracts/MultiInheritance.sol
+++ contracts/MultiInheritance.sol
@@ -5,9 +5,11 @@ pragma solidity ^0.8.4;
 contract A {
 
     string public name;
+    string public name2;
 
-    constructor(string memory _name) {
+    constructor(string memory _name, string memory _name2) {
         name = _name;
+        name2 = _name2;
     }
 
 
@@ -29,16 +31,16 @@ contract B  {
     }
 }
 
-contract  C is A('A'), B('helloWorld') {
+contract  C is A("A1", "A2"), B('helloWorld') {
 
 }
 
-contract  D is A, B {
-    // constructor() A("A") B("HelloWorld"){
+// contract  D is A, B {
+    // constructor() A("A1", "A2") B("HelloWorld"){
     //     // helloworld = _helloworld;
     // }
-    constructor(string memory _name, string memory _helloworld) A(_name) B(_helloworld){
+//     constructor(string memory _name, string memory _helloworld) A(_name, _name2) B(_helloworld){
 
-    }
+//     }
 
-}
+// }
