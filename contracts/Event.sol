acf1bf496a7bbaed2dd21c07e186187f632069e1
Alexzzl <805119233@qq.com>
2022-02-25 18:24:12 +0800

    event global ariables

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -8,5 +8,7 @@ contract Event {
     function transfer() public {
 
         emit log(msg.sender, "I Send 1 ETH to You");
+        emit log(msg.sender, "I Send 2 ETH to You");
+        emit log(msg.sender, "I Send 3 ETH to You");
     }
 }
