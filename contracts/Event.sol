9f07502445e8593c6ed40292d9dd059cfd0c9a21
Alexzzl <805119233@qq.com>
2022-02-25 17:05:50 +0800

    event

---
+++ contracts/Event.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract Event {
+    event log(address sender, string message);
+
+    function transfer() public {
+
+        emit log(msg.sender, "I Send 1 ETH to You");
+    }
+}
