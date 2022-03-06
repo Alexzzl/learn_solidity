54be1afab7a890bf053f3a4e1a48f0a3893498a3
Alexzzl <805119233@qq.com>
2022-03-06 10:12:11 +0800

    error

---
+++ contracts/Error.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+// require
+// assert
+// revert
+
+pragma solidity ^0.8.4;
+
+contract Error {
+    int public balance;
+
+    function deposit(int _amount) public {
+        // require(_amount > 0, "Deposited amount must be greater than zero");
+        int oldBalance = balance;
+
+        balance += _amount;
+
+        // assert(balance >= oldBalance);
+        if(balance < oldBalance){
+            revert("Impossible!");
+        }
+
+
+    }
+}
