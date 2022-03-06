f827e68bc407539acad66740cea4e9492422e610
Alexzzl <805119233@qq.com>
2022-03-07 00:05:04 +0800

    Modifier

---
+++ contracts/12_Modifier.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract Modifier {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(owner == msg.sender, "Not Owner");
+        _;
+    }
+
+    function onlyCallByOwner()  public onlyOwner view returns (bool) {
+        if(msg.sender == owner) {
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+
+    function adminOperation() public onlyOwner {
+        // require(owner == msg.sender, "Not Owner");
+    }
+
+    function withdraw() public onlyOwner {
+        // require(owner == msg.sender, "Not Owner");
+    }
+}
